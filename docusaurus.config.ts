f2e84d5b2e97407a9c6d1dc6c676fd9f26aff56c
ward727 <Ward727a@gmail.com>
2025-10-07 15:23:39 +0200

    init docusaurus and workflows
    Signed-off-by: ward727 <Ward727a@gmail.com>

---
+++ docusaurus.config.ts
@@ -0,0 +1,161 @@
+import {themes as prismThemes} from 'prism-react-renderer';
+import type {Config} from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
+const config: Config = {
+  title: 'RPG Creator - Documentation',
+  tagline: 'Official RPG Creator Documentation',
+  favicon: 'img/favicon.ico',
+
+  // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
+  future: {
+    v4: true, // Improve compatibility with the upcoming Docusaurus v4
+  },
+
+  // Set the production url of your site here
+  url: 'https://doc.rpgcreator.dev/',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'Ward727a', // Usually your GitHub org/user name.
+  projectName: 'RPGCreator', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'fr'],
+  },
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          sidebarPath: './sidebars.ts',
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        blog: {
+          showReadingTime: true,
+          feedOptions: {
+            type: ['rss', 'atom'],
+            xslt: true,
+          },
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          // Useful options to enforce blogging best practices
+          onInlineTags: 'warn',
+          onInlineAuthors: 'warn',
+          onUntruncatedBlogPosts: 'warn',
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    image: 'img/docusaurus-social-card.jpg',
+    colorMode: {
+      respectPrefersColorScheme: true,
+    },
+    navbar: {
+      title: 'RPG Creator',
+      logo: {
+        alt: 'RPG Creator Logo',
+        src: 'img/logo.png',
+      },
+      items: [
+        {
+          type: 'docSidebar',
+          sidebarId: 'tutorialSidebar',
+          position: 'left',
+          label: 'Documentation',
+        },
+        {
+          href: 'https://github.com/Ward727a/rpgcreator-web',
+          label: 'GitHub',
+          position: 'right',
+        },
+        {
+          type: 'localeDropdown',
+          position: 'right'
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Tutorial',
+              to: '/docs/intro',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Discord',
+              href: 'https://discord.gg/4yfq4NNzs4',
+            },
+            {
+              label: 'Mastodon',
+              href: 'https://mastodon.social/@RPGCreator',
+            },
+            {
+              label: 'Bluesky',
+              href: 'https://bsky.app/profile/rpgcreator.bsky.social',
+            },
+            {
+              label: 'Itch.io',
+              href: 'https://ward727.itch.io/rpg-creator',
+            },
+            {
+              label: 'X',
+              href: 'https://x.com/RPG_Creator_',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'GitHub - Engine',
+              href: 'https://github.com/Ward727a/RPGCreator'
+            },
+            {
+              label: 'GitHub - Documentation',
+              href: 'https://github.com/Ward727a/rpgcreator-web',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} RPG Creator. Built with Docusaurus.`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
---
+++ src/components/HomepageFeatures/index.tsx
@@ -0,0 +1,79 @@
+import type {ReactNode} from 'react';
+import clsx from 'clsx';
+import Heading from '@theme/Heading';
+import styles from './styles.module.css';
+
+type FeatureItem = {
+  title: string;
+  Svg: React.ComponentType<React.ComponentProps<'svg'>>;
+  description: ReactNode;
+};
+
+const FeatureList: FeatureItem[] = [
+  {
+    title: 'Open-source',
+    description: (
+      <>
+        RPG Creator is fully open-source, meaning that you can see and edit,
+        the <a href="https://github.com/Ward727a/RPGCreator">engine source code</a>, and yes, even this <a href="https://github.com/Ward727a/rpgcreator-web">website is open-source</a>!
+      </>
+    ),
+  },
+  {
+    title: 'Free',
+    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    description: (
+      <>
+        RPG Creator is totally free, and will always be.<br/>
+        No extension, update, or tool developped by us will be locked behind a paywall.
+      </>
+    ),
+  },
+  {
+    title: 'Modular',
+    description: (
+      <>
+        RPG Creator is developped to be modular, so if you want to add your own,
+        source code, you can without having to rebuild the entire engine!
+      </>
+    ),
+  },
+];
+/*
+ * For now I removed the image for feature section, as we don't have any picture to set inside
+ *
+ * code:
+ *
+ *  <div className="text--center">
+ *    <Svg className={styles.featureSvg} role="img" />
+ *  </div>
+ *
+ */
+function Feature({title, Svg, description}: FeatureItem) {
+  return (
+    <div className={clsx('col col--4')}>
+      <div className="text--center padding-horiz--md">
+        <Heading as="h3">{title}</Heading>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
+
+export default function HomepageFeatures(): ReactNode {
+  return (
+    <section className={styles.features}>
+      <div className="container">
+        <div className="text--center padding-horiz--md">
+          <Heading as="h2">What define RPG Creator</Heading>
+        </div>
+        <hr/>
+        <div className="row">
+          {FeatureList.map((props, idx) => (
+            <Feature key={idx} {...props} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
